ec78659f8614ad204bfe45a7b0879798cec01d3b
Ben Copeland <bencopeland911@gmail.com>
2016-01-27 15:04:08 -0600

    iono

---
+++ german.js
@@ -0,0 +1,17 @@
+var Translator = (function(trans) {
+
+	var germanLexicon = {
+		merry: "fröhlich",
+		christmas: "weihnachten",
+		and: "und",
+		happy: "glücklich",
+		new: "neu",
+		year: "jahr"
+	};
+
+	trans.toGerman = function () {
+		console.log("testing...");
+	};
+	
+	return trans;
+})(Translator);
