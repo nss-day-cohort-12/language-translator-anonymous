0b296e04e782720485adcf500e8d5e349b056a72
williamhickok11 <williamhickok11@gmail.com>
2016-01-27 15:32:48 -0600

    full working translator

--- glue.js
+++ glue.js
@@ -7,15 +7,33 @@ var Translator = (function() {
 })();
 
 
-var exicute = function (){
+var execute = function (){
   var userText = document.getElementById("textArea").value;
   userTextArray = userText.split(" ");
   console.log("User Input", userTextArray);
   
-  var finalTranslation = Translator.toIcelandic(userTextArray);
-  console.log("finalTranslation", finalTranslation);
+  var languageSelected = document.getElementById("selectLanguage").value
+  
+  if (languageSelected === "French"){
+    var finalTranslation = Translator.toFrench(userTextArray);
+    console.log("finalTranslation", finalTranslation);
+    document.getElementById("output").innerHTML = finalTranslation;
+    
+  } else if (languageSelected === "German") {
+    var finalTranslation = Translator.toGerman(userTextArray);
+    console.log("finalTranslation", finalTranslation);
+    document.getElementById("output").innerHTML = finalTranslation;
 
+  } else if (languageSelected === "Icelandic") {
+    var finalTranslation = Translator.toIcelandic(userTextArray);
+    console.log("finalTranslation", finalTranslation);
+    document.getElementById("output").innerHTML = finalTranslation;
+    
+  } else {
+    alert("Please Pick a language.")
   }
-document.getElementById("tranlate_button").addEventListener("click", exicute)
+};
+
+document.getElementById("tranlate_button").addEventListener("click", execute)
 
 
