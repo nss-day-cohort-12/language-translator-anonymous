ff9e85721c389c7923037e069b164f8c0e83e9d5
williamhickok11 <williamhickok11@gmail.com>
2016-01-28 09:18:03 -0600

    different way of french2

--- glue.js
+++ glue.js
@@ -1,19 +1,17 @@
 var Translator = (function() {
-  
-
-  return {
-
-	}
+  return {}
 })();
 
-
 var execute = function (){
-  var userText = document.getElementById("textArea").value;
+  //*** GET USER INPUT AND CREATE AN ARRAY ***//
+  var userText = document.getElementById("textArea").value.toLowerCase();
   userTextArray = userText.split(" ");
   console.log("User Input", userTextArray);
   
+  //*** ACCESS DROPDOWN VALUE ***//
   var languageSelected = document.getElementById("selectLanguage").value
   
+  //*** CHECK DROPDOWN AND RUN TRANSLATE FUNCTION ***//
   if (languageSelected === "French"){
     var finalTranslation = Translator.toFrench(userTextArray);
     console.log("finalTranslation", finalTranslation);
@@ -28,12 +26,22 @@ var execute = function (){
     var finalTranslation = Translator.toIcelandic(userTextArray);
     console.log("finalTranslation", finalTranslation);
     document.getElementById("output").innerHTML = finalTranslation;
-    
+
   } else {
     alert("Please Pick a language.")
   }
 };
 
-document.getElementById("tranlate_button").addEventListener("click", execute)
+document.getElementById("tranlate_button").addEventListener("click", execute);
 
+var clear = function() {
+  console.log("anything");
+  document.getElementById("textArea").value = "";
+  document.getElementById("output").innerHTML = "";
+}
+document.getElementById("clear").addEventListener("click", clear);
+
+
+
+// finalTranslation.slice(0,1).toUpperCase() += finalTranslation.slice(1);
 
